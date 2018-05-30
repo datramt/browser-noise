8aa4483fc7f647bf6062a73d11af6f1a2de0ee6e
Dan Tramte <datramt@gmail.com>
2018-05-30 13:11:13 -0400

    using codepen now

--- src/pages/Tutorial_CODE/project1.js
+++ src/pages/Tutorial_CODE/project1.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { zenburn } from 'react-syntax-highlighter/styles/hljs';
-import Iframe from 'react-iframe'
+import Iframe from 'react-iframe';
+import Codepen from 'react-codepen-embed';
 // import tutorials from './Tutorial_CODE/tutorials';
+
+const CodepenEmbedded = () => {
+  return (
+    <Codepen
+      hash="JyxeVP"
+      user="shettypuneeth"
+    />
+  );
+}
 
 const Tut1 = () => {
   return <SyntaxHighlighter language='javascript' style={zenburn}>{`let mrNoisy = new p5.Noise();
@@ -283,17 +293,60 @@ function touchStarted() {
 //     }
 // }
 
+// <Iframe url="https://datramt.github.io/browser-noise-Project-1/"
+//     width="750px"
+//     height="220px"
+//     id="myId"
+//     className="myClassname"
+//     display="initial"
+//     position="relative"
+//     scrolling="no"
+//     allowFullScreen/>
+// <hr/>
+
 class Project1 extends Component {
-
-
-
     render() {
         return (
             <div>
             <h3>{"Make noise in your browser using p5.js sound library"}</h3>
-            <Iframe url="https://datramt.github.io/browser-noise-Project-1/"
+
+            <h4>{"TUTORIAL 1"}</h4>
+            <p>{"make an account at "}<a href="http://alpha.editor.p5js.org">p5.js web editor</a>{" and follow along:"}</p>
+            <p>{"declare and assign a p5 noise object to a variable called 'mrNoisy', then within the setup function, set the noise type and amplitude by calling methods .setType() and .amp(). Then call the .start() method."}</p>
+            <p>{"see how this is done below by pressing 'Run Pen', although, do note that I have commented out the mrNoisy.start() to spare your ears ;)"}</p>
+            <Codepen
+              hash="PawjEa"
+              themeId="Panda"
+              user="datramt"
+              height="220px"
+              defaultTab="js"
+              loader={() => <div>Loading...</div>}
+            />
+            <p>{"congratulations, you have just made noise in the web browser ;) See tutorial video below"}</p>
+            <Iframe url="https://www.youtube.com/embed/Rpl2-BEsX5M"
                 width="750px"
-                height="220px"
+                height="422px"
+                id="myId"
+                className="myClassname"
+                display="initial"
+                position="relative"
+                scrolling="no"
+                allowFullScreen/ >
+            <hr/>
+            <h4>{"TUTORIAL 2"}</h4>
+            <p>{"Using the p5.Dom library, add buttons to turn on and off the noise. Position the buttons, and define the event listeners"}</p>
+            <p>{"If you want to see a live demo of this code, simply hit the RESULT tab in the codepen below"}</p>
+                <Codepen
+                  hash="RJNgmJ"
+                  user="datramt"
+                  themeId="Panda"
+                  height="400px"
+                  defaultTab="js"
+                  loader={() => <div>Loading...</div>}
+                />
+            <Iframe url="https://www.youtube.com/embed/OjBcx7OVdCI"
+                width="750px"
+                height="422px"
                 id="myId"
                 className="myClassname"
                 display="initial"
@@ -301,64 +354,63 @@ class Project1 extends Component {
                 scrolling="no"
                 allowFullScreen/>
             <hr/>
-            <h4>{"TUTORIAL 1"}</h4>
-
-            <p>make an account at <a href="http://alpha.editor.p5js.org">p5.js web editor</a> and follow along:</p>
-            <p>{"declare and assign a p5 noise object to a variable called 'mrNoisy', then within the setup function, set the noise type and amplitude by calling methods .setType() and .amp(). Then call the .start() method."}</p>
-            <Tut1/>
-            <p>{"congratulations, you have just made noise in the web browser ;) See tutorial below"}</p>
-            <Iframe url="https://www.youtube.com/embed/Rpl2-BEsX5M"
-        width="750px"
-        height="422px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        scrolling="no"
-        allowFullScreen/>
-    <hr/>
-        <h4>{"TUTORIAL 2"}</h4>
-        <p>{"Using the p5.Dom library, add buttons to turn on and off the noise. Position the buttons, and define the event listeners"}</p>
-        <Tut2/>
-            <Iframe url="https://www.youtube.com/embed/OjBcx7OVdCI"
-        width="750px"
-        height="422px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        scrolling="no"
-        allowFullScreen/>
-    <hr/>
-        <h4>{"TUTORIAL 3"}</h4>
-        <p>{"Create two more dom elements: a dropdown menu select, and a slider. Define the options of the dropdown menu by calling the .option() method. Use a .changed() event listener for the menu, and a .input() event listener for the slider. Pass respective values into mrNoisy methods, .setType(), and .amp()"}</p>
-        <Tut3/>
+            <h4>{"TUTORIAL 3"}</h4>
+            <p>{"Create two more dom elements: a dropdown menu select, and a slider. Define the options of the dropdown menu by calling the .option() method. Use a .changed() event listener for the menu, and a .input() event listener for the slider. Pass respective values into mrNoisy methods, .setType(), and .amp()"}</p>
+            <Codepen
+              hash="wXBqGL"
+              user="datramt"
+              themeId="Panda"
+              height="700px"
+              defaultTab="js"
+              loader={() => <div>Loading...</div>}
+            />
             <Iframe url="https://www.youtube.com/embed/wJr5h5wjkoc"
-        width="750px"
-        height="422px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        scrolling="no"
-        allowFullScreen/>
-    <hr/>
-        <h4>{"TUTORIAL 4"}</h4>
-        <p>{"See video regarding... what is even noise?"}</p>
-        <p>{"video only (coming soon)"}</p>
-        <hr/>
-        <h4>{"TUTORIAL 5"}</h4>
-        <p>{"Declare and assign a p5.FFT() object to a variable, fft, in order analyze the sound. fft.analyze() will output an array containing 1024 values, each representing a frequency bin of the audio spectrum. Use a for loop to loop through the array, and plot that data onto the canvas."}</p>
-        <Tut5/>
-        <hr/>
-        <h4>{"TUTORIAL 6"}</h4>
-        <p>{"Adjust the volume slider so as to make its input in units of decibels using the formula"} <code>amplitude = 10^(decibels/20)</code>{". Take the log of the spectrum index in order to plot the freqency logarithmically."}</p>
-        <Tut6/>
-        <hr/>
-        <h4>{"TUTORIAL 7"}</h4>
-        <p>{"Rather than drawing points, we can connect each point by changing 'point' to 'vertex' and wrapping a beginShape() endShape(). Let's now make it so that when the user changes the color of the noise, that color string is also passed into the fill() function. Finally, we'll add an event listener to resume the audio context for browser that suspend them automatically."}</p>
-        <Tut7/>
-        </div>
+                width="750px"
+                height="422px"
+                id="myId"
+                className="myClassname"
+                display="initial"
+                position="relative"
+                scrolling="no"
+                allowFullScreen/>
+            <hr/>
+            <h4>{"TUTORIAL 4"}</h4>
+            <p>{"See video regarding... what is even noise?"}</p>
+            <p>{"video only (coming soon)"}</p>
+            <hr/>
+            <h4>{"TUTORIAL 5"}</h4>
+            <p>{"Declare and assign a p5.FFT() object to a variable, fft, in order analyze the sound. fft.analyze() will output an array containing 1024 values, each representing a frequency bin of the audio spectrum. Use a for loop to loop through the array, and plot that data onto the canvas."}</p>
+            <Codepen
+              hash="NzPvpP"
+              user="datramt"
+              themeId="Panda"
+              height="700px"
+              defaultTab="js"
+              loader={() => <div>Loading...</div>}
+            />
+            <hr/>
+            <h4>{"TUTORIAL 6"}</h4>
+            <p>{"Adjust the volume slider so as to make its input in units of decibels using the formula"} <code>amplitude = 10^(decibels/20)</code>{". Take the log of the spectrum index in order to plot the freqency logarithmically."}</p>
+            <Codepen
+              hash="ERawje"
+              themeId="Panda"
+              user="datramt"
+              height="700px"
+              defaultTab="js"
+              loader={() => <div>Loading...</div>}
+            />
+            <hr/>
+            <h4>{"TUTORIAL 7"}</h4>
+            <p>{"Rather than drawing points, we can connect each point by changing 'point' to 'vertex' and wrapping a beginShape() endShape(). Let's now make it so that when the user changes the color of the noise, that color string is also passed into the fill() function. Finally, we'll add an event listener to resume the audio context for browser that suspend them automatically."}</p>
+            <Codepen
+              hash="bKNoxY"
+              themeId="Panda"
+              user="datramt"
+              height="700px"
+              defaultTab="js"
+              loader={() => <div>Loading...</div>}
+            />
+            </div>
         )
     }
 }
